e6051d47c0c802e0fdac2bd52c56fb4358c773b7
Paul2hip8 <parescigno@gmail.com>
2023-11-19 13:15:58 -0500

    working test.py

--- params.py
+++ params.py
@@ -3,7 +3,7 @@ class Parameters():
 
 		self.data_dir =  {'KITTI': './data/KITTI_dataset/dataset',}
 		self.test_video = {'KITTI': {'KITTI': ['03', '04', '05', '06', '07', '10']}}
-		self.n_processors = 16
+		self.n_processors = 12 #changed from 16 to 12 for Paul's PC
 		self.scale = 0.7
 		self.img_w = 640  
 		self.img_h = 384   
